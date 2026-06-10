72c0f422a76e6fbeb258fd76f1200f877cd4869f
estanislaomolina <estanislaomolina05@gmail.com>
2026-06-10 15:30:33 -0300

    download dataset

---
+++ data/coco/download.py
@@ -0,0 +1,21 @@
+import os
+import urllib.request
+
+# COCO download URLs
+FILES = {
+    "annotations_trainval2017.zip":
+        "http://images.cocodataset.org/annotations/annotations_trainval2017.zip",
+    "val2017.zip":
+        "http://images.cocodataset.org/zips/val2017.zip",
+}
+
+download_dir = "."
+
+for filename, url in FILES.items():
+    output_path = os.path.join(download_dir, filename)
+
+    print(f"Downloading {filename}...")
+    urllib.request.urlretrieve(url, output_path)
+    print(f"Saved to {output_path}")
+
+print("All downloads completed.")
